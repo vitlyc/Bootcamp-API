683607e9db8850723e51ae589e23621bedc9ce8a
vitlyc <72764133+vitlyc@users.noreply.github.com>
2022-04-16 18:25:07 +0300

    add middleware/advancedResults

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,10 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload,
 } = require("../controllers/bootcamps");
+
+const Bootcamp = require("../models/Bootcamp");
+const advancedResults = require("../middleware/advancedResults");
+
 //Include other resource routers
 const courseRouter = require("./courses");
 
@@ -19,7 +23,10 @@ router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router.route("/:id/photo").put(bootcampPhotoUpload);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
 router.route("/:id").get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
 module.exports = router;
